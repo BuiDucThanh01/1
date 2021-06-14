fa760d3cc6985246f7e6fc4b1aaf40c03d97ed3c
Bùi Đức Thành <85858562+BuiDucThanh01@users.noreply.github.com>
2021-06-14 12:43:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,4 @@ const CONFIG = {
     mess: 'Chúc em luôn vui vẻ thi được điểm cao 😊',
     messDesc: 'Mong Wed giúp e giải trí thi tốt hơn',
     btnAccept: 'em cám ơn Anh cũng thế :))',
-    messLink: 'http://fb.com' https://www.facebook.com/profile.php?id=100010045690226
-}
+    messLink: 'https://www.facebook.com/profile.php?id=100010045690226' 
