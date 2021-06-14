d0573c4d182227683cfa772569411d57314c2596
Bùi Đức Thành <85858562+BuiDucThanh01@users.noreply.github.com>
2021-06-14 11:55:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Hà ơi!(Tặng em)',
+    introTitle: 'Hà ơi!(Tặng em 1 chút vui vẻ)',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
